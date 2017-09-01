c2e3e5379915e79d781f0db8fd984852f3ee238d
deepak4175 <30748810+deepak4175@users.noreply.github.com>
2017-09-01 14:32:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,12 +4,51 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+ 
+ var articleone= {
+  title: 'this is the title of article one',
+  head: '<h> this is the heading of the article one</h>',
+  content: '<p> A bench comprising Chief Justice Dipak Misra and Justices A M Khanwilkar and D Y Chandrachud considered the submissions of lawyer Prashant Bhushan that the plea required urgent hearing in view of the decision of the government to send Rohingyas back to their native land.</p>  '
+ }
+ function newtemplate(data){
+ var Title=data.title;
+ var Head=data.head;
+ var Content=data.content;
+ var Templateone=`<html>
+    <head>
+         <link href="/ui/style.css" rel="stylesheet" />
+        <title>
+           ${Title}
+            
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">home</a>
+             <hr/>
+         <h1>
+           ${Head}
+        </h1>
+       ${Content}
+      
+        </div>
+            
+       </div>
+    </body>
+  </html>`;
+  
+  return newtemplate;
+  
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res){
-        res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+        res.send(newTemplate(articleone));
 });
 app.get('/article-two', function (req, res){
         res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
