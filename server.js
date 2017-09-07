ac14b4584fc79de86bc5a3b733a7c7d406926698
deepak4175 <30748810+deepak4175@users.noreply.github.com>
2017-09-07 22:05:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,7 +72,6 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
- 
 });
 
 app.get('/ui/main.js', function (req, res) {
